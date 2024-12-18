59eca7b24aaf0b867b1328ccdfab8b8e1b1531ad
JoGyoungJun <yoop80075@gmail.com>
2024-12-18 10:36:47 +0900

    쿼리 수정

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZonedDateTime;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "SELECT u.Remaining FROM User u")
-    ZonedDateTime findRemainingByUserId(Long id);
+    @Query(value = "SELECT u.Remaining FROM User u where u.id = :id")
+    ZonedDateTime findRemainingByUserId(@Param("id") Long id);
 }
